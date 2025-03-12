8bb20b07028b04b48b2c0c4a8f3f35e019d1c753
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-12 17:07:30 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import ScrollToTop from "./components/ScrollToTop";
-import Layout from "./Layout/Layout";
-import Home from "./pages/Home/Home";
-import ContactUs from "./pages/ContactUs/ContactUs";
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword/ResetPassword";
-import AboutUs from "./pages/AboutUs/AboutUs";
-import ProductDetails from "./pages/ProductDetail/ProductDetails";
-import ProductListing from "./pages/ProductListing/ProductListing";
-import Error from "./pages/Error/Error";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Profile from "./pages/Profile/Profile";
-import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
-import WishList from "./pages/WishList/WishList";
+import Layout from "./Layout/Layout.tsx";
+import Home from "./pages/Home/Home.tsx";
+import ContactUs from "./pages/ContactUs/ContactUs.tsx";
+import Login from "./pages/Login/Login.tsx";
+import SignUp from "./pages/SignUp/SignUp.tsx";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.tsx";
+import ResetPassword from "./pages/ResetPassword/ResetPassword.tsx";
+import AboutUs from "./pages/AboutUs/AboutUs.tsx";
+import ProductDetails from "./pages/ProductDetail/ProductDetails.tsx";
+import ProductListing from "./pages/ProductListing/ProductListing.tsx";
+import Error from "./pages/Error/Error.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import Profile from "./pages/Profile/Profile.tsx";
+import ShoppingCart from "./pages/ShoppingCart/ShoppingCart.tsx";
+import WishList from "./pages/WishList/WishList.tsx";
 
 function App() {
     return (
