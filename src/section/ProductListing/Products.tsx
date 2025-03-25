3ff8552d2984b5186f44227ba810e55325786818
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-25 17:46:04 +0530

    fetched searchedProducts

--- src/section/ProductListing/Products.tsx
+++ src/section/ProductListing/Products.tsx
@@ -1,8 +1,12 @@
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilteredProducts from "./FilteredProducts";
 import Filters from "./Filters";
+import { useLocation, useSearchParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
+import { getSearchedProducts } from "../../store/Slices/productSlice";
 
 interface ProductsProps {
     selectedCategory: string;
@@ -30,7 +34,15 @@ const Products: React.FC<ProductsProps> = ({
         [sortOptions[3], "price_low_to_high"],
     ]);
 
-    console.log("products comp:");
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    const { searchText } = location.state || {};
+
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        dispatch(getSearchedProducts(searchText));
+    }, [dispatchEvent, searchText]);
 
     return (
         <Box sx={{ py: "48px" }} maxWidth={"1100px"} mx={"auto"}>
@@ -44,7 +56,12 @@ const Products: React.FC<ProductsProps> = ({
                         display: { xs: "none", md: "block" },
                     }}
                 >
-                    <Filters selectedSort={selectedSort} sortBy={sortByMap} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
+                    <Filters
+                        selectedSort={selectedSort}
+                        sortBy={sortByMap}
+                        selectedCategory={selectedCategory}
+                        setSelectedCategory={setSelectedCategory}
+                    />
                 </Grid>
                 <Grid
                     item
