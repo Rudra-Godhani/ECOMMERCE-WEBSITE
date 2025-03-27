9bb54d162e6074167027d64f51aabbea5d7e7ea8
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-27 17:56:31 +0530

    change name

---
+++ src/store/Slices/Cart_Slice.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface CartItem {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    quantity: number;
+    image: string;
+    color: string;
+    availability: Boolean;
+    rating: number;
+    brand: string;
+    category: string;
+}
+
+const initialState: CartItem[] = [];
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: initialState,
+    reducers: {
+        addProductToCart: (state, action: PayloadAction<CartItem>) => {
+            state.push(action.payload);
+        },
+        removeProductFromCart: (state, action: PayloadAction<string>) => {
+            return state.filter((item) => item.id !== action.payload);
+        },
+        increaseQuantity: (state, action: PayloadAction<string>) => {
+            const existingProduct = state.find(
+                (item) => item.id === action.payload
+            );
+            if (existingProduct) {
+                existingProduct.quantity += 1;
+            }
+        },
+        decreaseQuantity: (state, action: PayloadAction<string>) => {
+            const existingProduct = state.find(
+                (item) => item.id === action.payload
+            );
+            console.log(existingProduct?.quantity);
+            if (existingProduct && existingProduct.quantity > 1) {
+                existingProduct.quantity -= 1;
+                console.log(existingProduct?.quantity);
+            } else {
+                return state.filter((item) => item.id !== action.payload);
+            }
+        },
+    },
+});
+
+export const {
+    addProductToCart,
+    removeProductFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+} = cartSlice.actions;
+export default cartSlice.reducer;
