d209ea7988cdf0c5b02ebcf4a0567731c4c60bfd
Rudra-Godhani <rudragodhani12@gmail.com>
2025-03-28 17:54:33 +0530

    fetched cartdata from backened

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CartData from "../../section/ShoppingCart/CartData";
 import CartTotal from "../../section/ShoppingCart/CartTotal";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
+import { getCart } from "../../store/Slices/CartSlice";
 
 const ShoppingCart: React.FC = () => {
+    const dispacth = useDispatch<AppDispatch>();
+    useEffect(() => {
+        dispacth(getCart());
+    }, [dispacth]);
     return (
         <>
             <CartData />
